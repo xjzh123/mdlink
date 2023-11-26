810aa0cb797e59d7ca073463791be839e52ad2db
xjzh123 <37945610+xjzh123@users.noreply.github.com>
2023-11-26 20:23:57 +0800

    add indent support

--- main.js
+++ main.js
@@ -21,6 +21,31 @@ if (url_content) {
 }
 
 editorEl.addEventListener('input', (e) => {
+  console.debug('Input Event: ', e)
+
+  if (e.inputType === 'insertLineBreak' || e.data === '\n') {
+    if (editorEl.selectionStart === editorEl.selectionEnd) {
+      let i = editorEl.selectionStart - 1
+      while (i--) {
+        if (editorEl.value.charAt(i) === '\n') {
+          break
+        }
+      }
+      let j = i
+      while (j++) {
+        if (editorEl.value.charAt(j) !== ' ') {
+          break
+        }
+      }
+
+      // editorEl.value = editorEl.value.slice(0, editorEl.selectionStart) + ' '.repeat(j - i - 1) + editorEl.value.slice(editorEl.selectionStart, -1)
+
+      document.execCommand('insertText', false, ' '.repeat(j - i - 1))
+    }
+  }
+})
+
+editorEl.addEventListener('input', (e) => {
   const content = e.target.value
 
   contentEl.innerHTML = md.render(content)
